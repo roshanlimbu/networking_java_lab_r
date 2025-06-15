4a1271cd0a6670ee9889342ef13b4c32991e56a4
roshanlimbu <topazkayle@gmail.com>
2025-06-15 13:37:00 +0545

    lab 14

---
+++ lab14/main.java
@@ -0,0 +1,23 @@
+import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class main {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://localhost:80");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            StringBuilder response = new StringBuilder();
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+            in.close();
+            System.out.println("Response: " + response.toString());
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
